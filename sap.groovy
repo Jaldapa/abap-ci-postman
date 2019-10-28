822aeaeccf928947bfc4c10181e15660de1c1cd6
Jaldapa <56886740+Jaldapa@users.noreply.github.com>
2019-10-28 18:14:55 +0100

    Update sap.groovy

--- sap.groovy
+++ sap.groovy
@@ -31,7 +31,7 @@ def abap_sci(LABEL,HOST,CREDENTIAL,PACKAGE,VARIANT) {
 	withCredentials([usernamePassword(credentialsId: CREDENTIAL, usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {	
 		stage('[' + LABEL + '] ABAP Code Inspector') {
 			dir('sap-pipeline') {
-					bat "newman run abap_sci.postman_collection.json --insecure --bail " +
+					sh "newman run abap_sci.postman_collection.json --insecure --bail " +
 					"--environment NPL.postman_environment.json " +
 					"--timeout-request 120000 " +
 					"--global-var host=$HOST " +
